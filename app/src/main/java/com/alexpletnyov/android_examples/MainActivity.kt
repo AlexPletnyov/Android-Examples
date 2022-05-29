44c638ee40ffe28c9b6d4624b83d6d1919b407c6
Alexander Pletnyov <pletnevlive@gmail.com>
2022-05-30 03:46:21 +0900

    Basic setup

--- app/src/main/java/com/alexpletnyov/android_examples/MainActivity.kt
+++ app/src/main/java/com/alexpletnyov/android_examples/MainActivity.kt
@@ -2,10 +2,21 @@ package com.alexpletnyov.android_examples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+
 		setContentView(R.layout.activity_main)
+
+		val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+		recyclerView.layoutManager = LinearLayoutManager(this)
+		recyclerView.adapter = CustomRecyclerAdapter(getCatList())
+	}
+
+	private fun getCatList(): List<String> {
+		return this.resources.getStringArray(R.array.cat_names).toList()
 	}
 }
